92938c86486a0a590b50cf776fecd0d1333b4dd8
Taichi Murai <taichimurai0514@gmail.com>
2021-07-31 13:53:00 +0900

    aa

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,5 @@
-import puppeteer from 'puppeteer';
 import * as dotenv from 'dotenv';
+import puppeteer from 'puppeteer';
 
 dotenv.config();
 const EMAIL = process.env.EMAIL ?? '';
@@ -15,14 +15,23 @@ const PASSWORD = process.env.PASSWORD ?? '';
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
   );
 
-  await page.goto('https://moneyforward.com/');
+  await page.goto('https://moneyforward.com/', {
+    waitUntil: 'load',
+    timeout: 0,
+  });
   await page.click('a[href="/sign_in"]');
-  await page.goto(page.url());
+  await page.goto(page.url(), {
+    waitUntil: 'load',
+    timeout: 0,
+  });
   const signinUrl = await page.evaluate(() =>
     Array.from(document.querySelectorAll('.buttonWrapper a:nth-child(1)'), a => a.getAttribute('href'))
   );
 
-  await page.goto(`https://id.moneyforward.com${signinUrl[0]}`);
+  await page.goto(`https://id.moneyforward.com${signinUrl[0]}`, {
+    waitUntil: 'load',
+    timeout: 0,
+  });
 
   await page.type('input[type="email"]', EMAIL!);
   await page.click('input[type="submit"]');
@@ -30,14 +39,20 @@ const PASSWORD = process.env.PASSWORD ?? '';
   await page.waitForNavigation({
     timeout: 60000,
     waitUntil: 'domcontentloaded',
-  });
+  }).catch((e) => {
+      throw new Error('timeout');
+   });
 
   await page.type('input[type="password"]', PASSWORD!);
   await page.click('input[type="submit"]');
-  await page.waitForNavigation({
-    timeout: 60000,
-    waitUntil: 'domcontentloaded',
-  });
+  await page
+    .waitForNavigation({
+      timeout: 60000,
+      waitUntil: 'domcontentloaded',
+    })
+    .catch(e => {
+      throw new Error('timeout');
+    });;
   await page.click('a[href="/aggregation_queue/bq3Jl7efNG-zqQHrZXWAQQ"]');
 
   await browser.close();
